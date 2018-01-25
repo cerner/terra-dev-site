aaecdef0cb1a5e9913c1d2e1b9cb06c4d0709430
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-01-25 12:48:46 -0600

    Ooops.

--- src/config/webpack.prod.config.js
+++ src/config/webpack.prod.config.js
@@ -1,20 +1,17 @@
 // By default eslint assumes packages imported are supposed to be dependencies,
 // not devDependencies. Disabling this rule in webpack.conig.js
 /* eslint-disable import/no-extraneous-dependencies */
-const config = require('./webpack.config');
+const webpackConfig = require('./webpack.config');
 const CleanPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
-const config = {
-  // Clean build before running
-  plugins: [
-    new CleanPlugin('build', { exclude: ['stats.json'] }),
-  ],
-  // Create output file
-  output: {
-    path: path.resolve(path.join(process.cwd(), 'build')),
-    filename: '[name]-[hash].js',
-  },
+// Clean build before running
+webpackConfig.plugins.push(new CleanPlugin('build', { exclude: ['stats.json'] }));
+
+// Create output file
+webpackConfig.output = {
+  path: path.resolve('build'),
+  filename: '[name]-[hash].js',
 };
 
-module.exports = config;
+module.exports = webpackConfig;
