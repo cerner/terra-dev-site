b45a1fabefabb54e04c952f33375116d53932fd8
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-01-25 09:06:03 -0600

    No longer needed. Offloaded what I wanted to the night watch config.

--- scripts/start-terra-site/start.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const shell = require('shelljs');
-
-const webpackConfigPath = path.resolve(__dirname, '..', '..', 'src', 'config', 'webpack.config');
-
-shell.exec(`webpack-dev-server --config ${webpackConfigPath} --progress`);
