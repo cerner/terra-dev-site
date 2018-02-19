9bc64bfede336ce5b8ac3d34577c91f090c2e478
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-02-19 11:28:34 -0600

    App Config docs.

---
+++ docs/AppConfig.md
@@ -0,0 +1,16 @@
+# Application Configuration
+Terra-site uses the app configuration to build the site application and enable the application utilities to switch the site's theme, locale and/or directionality. Terra Site package provides a [default app config](https://github.com/cerner/terra-site/blob/master/src/config/site.config.js#L17) that is easy to customize by overriding the default value.
+
+
+#### Application Configuration Options
+
+Name | Type | Description
+--- | ---  | ---
+logoSrc | string | The logo the site header should display. Defaults to undefined.
+title | string | The title the site header should display. Defaults to a blank title.
+themes | object | The themes to supply the ThemeProvider which allows the site to switch between themes. Providing multiple key-value pairs enables the theme utility to display in the header's toolbar. The key should be the theme name, while the value is the sass theme name.
+defaultTheme | string | The default theme of the site. **Note:** this value should be a key that was supplied to the `themes` object. The open-sourced theme is the default theme.
+locales | array of strings | The locales to supply Base with, which allows the site to switch between locales. By default, The [i18nSupportedLocales](https://github.com/cerner/terra-core/blob/master/packages/terra-i18n/src/i18nSupportedLocales.js) are provide.
+defaultLocale | string |  The default locale of the site. Defaults to 'en'.
+bidirectional | boolean |  Indicates if the site supports bidirectionally. If enabled, the directionality utility will display in the toolbar. Defaults to true.
+defaultDirection | string | The default direction of the site. Defaults to 'ltr'.
--- src/config/site.config.js
+++ src/config/site.config.js
@@ -1,52 +1,19 @@
-const i18nSupportedLocales = require('terra-i18n/lib/i18nSupportedLocales');
 const navigationConfig = require('./navigation.config');
+const appConfig = require('./app.config');
 
 module.exports = {
+  /* The application configuration.  */
+  appConfig,
+
+  /* The component configuration. */
+  componentConfigPath: {},
+
   /* The navigation configuration.  */
   navConfig: navigationConfig,
-
-  /* The path to the component configuration. */
-  componentConfigPath: './generatedComponentConfig.js',
 
   /* The image to display as page placeholder when a component does not render. */
   placeholderSrc: '',
 
   /* The README content to display on the home page. */
   readMeContent: '',
-
-  appConfig: {
-    /* The loge the site header should display. */
-    logoSrc: undefined, // maps to appLogoSrc
-
-    /* The title the site header should display. */
-    title: '', // maps to appTitle
-
-    /* The subtitle the site header should display. */
-    subtitle: '', // maps to appSubtitle
-
-    /** The themes to supply the ThemeProvider which allows the site to switch
-      * between themes. Providing multiple enables the theme utility to display
-      * in the header's toolbar.
-      */
-    themes: {
-      'Default Theme': '',
-    },
-
-    /* The default theme of the site. The open-sourced theme is the default theme. */
-    defaultTheme: 'Default Theme',
-
-    /** The locales to supply Base with, which allows the site to switch
-      * between locales. English is the default language.
-      */
-    locales: i18nSupportedLocales,
-
-    /** Indicates if the site supports bidirectionality. If enabled, the directionality
-      * utility will display in the toolbar.
-      */
-    bidirectional: true, // maps to hideBidiUtility
-
-
-    /* The default direction of the site. 'ltr' is the default direction. */
-    defaultDirection: 'ltr',
-  },
 };
