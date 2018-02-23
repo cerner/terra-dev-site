e252b87b3bc32bbc5b4bbc3ac1320df4129128dd
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2018-02-23 13:17:09 -0600

    Add Theming-plugin Jest Tests (#24)
    * Add theming-plugin tests.
    
    * Update jestsetup.js
    
    * Use react 16.

---
+++ jestsetup.js
@@ -0,0 +1,13 @@
+// Make Enzyme functions available in all test files without importing
+/* eslint-disable import/no-extraneous-dependencies */
+import Enzyme, { mount, render, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+const htmlTag = document.getElementsByTagName('html')[0];
+htmlTag.setAttribute('dir', 'ltr');
