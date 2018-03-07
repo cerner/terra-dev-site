a182149696c134422db8e522208a9556b10b744c
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-03-07 08:27:07 -0600

    Starting point.

---
+++ src/templates/SiteDocTemplate.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const propTypes = {
+};
+
+const defaultProps = {
+};
+
+class SiteDocTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+SiteDocTemplate.propTypes = propTypes;
+SiteDocTemplate.defaultProps = defaultProps;
+
+export default SiteDocTemplate;
