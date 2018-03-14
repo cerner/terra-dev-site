9cbedf9e2cf394aa8252156192602b3bd1c3780f
Matt Henkes <mjhenkes@gmail.com>
2018-03-14 14:15:19 -0500

    Create a placeholder site. (#59)
    * Create a placeholder site. Enable Heroku pr reviews and auto gh-pages deployment.
    
    * bye themeable-variables.json

---
+++ componentConfig.js
@@ -0,0 +1,66 @@
+// import your template documentation and/or examples here
+
+const navConfig = {
+  'getting-started': {
+    name: 'Building A Site',
+    path: '/building-a-site',
+  },
+  config: {
+    name: 'Configuration',
+    path: '/config',
+    docs: [
+      {
+        name: 'Application Configuration',
+        path: '/app-config',
+      },
+      {
+        name: 'Component Configuration',
+        path: '/component-config',
+      },
+      {
+        name: 'Navigation Configuration',
+        path: '/navigation-config',
+      },
+    ],
+  },
+  webpack: {
+    name: 'Webpack',
+    path: '/webpack',
+    docs: [
+      {
+        name: 'Configuration',
+        path: '/webpack-configs',
+      },
+      {
+        name: 'I18n Plugin',
+        path: '/i18n-plugin',
+      },
+      {
+        name: 'Theming Plugin',
+        path: '/theming-plugin',
+      },
+    ],
+  },
+  templates: {
+    name: 'Templates',
+    path: '/templates',
+    docs: [{
+      name: 'Index Page Template',
+      path: '/index-page-template',
+      // If you have one exmample, use the component key, if more then one example, add to the docs array and remove the component key.
+      // component: add your example here
+      // docs: [
+      //   {
+      //     name: // Menu Link Name
+      //     path: // The path for the 1st example
+      //   },
+      //   {
+      //     name: // Menu Link Name
+      //     path: // The path for the 2nd example
+      //   },
+      // ],
+    }],
+  },
+};
+
+export default navConfig;
