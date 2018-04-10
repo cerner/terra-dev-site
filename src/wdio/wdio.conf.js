cbd4f5f68b274094d2424f5ff8054348ebd4f620
Matt Henkes <mjhenkes@gmail.com>
2018-04-10 12:36:57 -0500

    default wdio config for terra-dev-site

---
+++ src/wdio/wdio.conf.js
@@ -0,0 +1,21 @@
+const wdioConf = require('terra-toolkit/lib/wdio/conf');
+const webpackConfig = require('../webpack/webpack.config');
+
+const config = {
+  ...wdioConf.config,
+
+  webpackConfig,
+
+  terra: {
+    selector: '[data-terra-dev-site-content] *:first-child',
+  },
+
+  beforeHook() {
+    // Being Terra tests are executed on an SPA, a full refresh is required
+    // in order to reset the site. This ensures customProperty tests and any
+    // other dom modifications are cleared before starting a test.
+    global.browser.refresh();
+  },
+};
+
+exports.config = config;
