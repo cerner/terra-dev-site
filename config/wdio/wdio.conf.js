70a11fc780589e2aab56a1249f46e0f32e7c1abe
Matt Henkes <mjhenkes@gmail.com>
2018-04-20 11:45:33 -0500

    add axe override config here

--- config/wdio/wdio.conf.js
+++ config/wdio/wdio.conf.js
@@ -10,6 +10,16 @@ const config = {
     selector: '[data-terra-dev-site-content] *:first-child',
   },
 
+  axe: {
+    inject: true,
+    options: {
+      rules: [{
+        id: 'landmark-one-main',
+        enabled: false,
+      }],
+    },
+  },
+
   beforeHook() {
     // Being Terra tests are executed on an SPA, a full refresh is required
     // in order to reset the site. This ensures customProperty tests and any
