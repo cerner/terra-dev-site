683c439bd78da55936ca16c696d225072e83e0f3
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2018-06-01 16:12:14 -0500

    Update wdio.conf.js

--- config/wdio/wdio.conf.js
+++ config/wdio/wdio.conf.js
@@ -13,10 +13,10 @@ const config = {
   axe: {
     inject: true,
     options: {
-      rules: [{
-        id: 'landmark-one-main',
-        enabled: false,
-      }],
+      rules: [
+        { id: 'landmark-one-main', enabled: false },
+        { id: 'page-has-one-heading', enabled: false },
+      ],
     },
   },
 
