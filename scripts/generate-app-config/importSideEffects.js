bcd9724fa5571e905adeea75a57c7315a9753d5e
Brett Jankord <bjankord@gmail.com>
2018-09-07 17:20:19 -0500

    Update linters (#108)
    * Update linters
    
    * Update changelog
    
    * Revert formatting changes to importSideEffects.js

--- scripts/generate-app-config/importSideEffects.js
+++ scripts/generate-app-config/importSideEffects.js
@@ -5,11 +5,14 @@ const ImportAggregator = require('./generation-objects/ImportAggregator');
 /**
 * Resolve relative paths that start with '.', then glob paths, then apply side effect imports
 */
+// Disabling linter to preserve more readable formatting
+/* eslint-disable */
 const importSideEffects = (sideEffects, imports) => {
   sideEffects.map(sideEffectGlob =>
     (sideEffectGlob.charAt(0) === '.' ? path.resolve('dev-site-config', sideEffectGlob) : sideEffectGlob)).reduce((acc, sideEffectGlob) =>
-    acc.concat(glob.sync(`${sideEffectGlob}?(.*)`)), []).forEach(sideEffectPath =>
-    imports.addImport(ImportAggregator.relativePath(sideEffectPath)));
+      acc.concat(glob.sync(`${sideEffectGlob}?(.*)`)), []).forEach(sideEffectPath =>
+        imports.addImport(ImportAggregator.relativePath(sideEffectPath)));
 };
+/* eslint-enable */
 
 module.exports = importSideEffects;
