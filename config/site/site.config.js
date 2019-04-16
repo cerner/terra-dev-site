48e53927cbdce6b746d903548594408c8aae2205
Matt Henkes <mjhenkes@gmail.com>
2019-04-16 14:04:40 -0500

    phrasing

--- config/site/site.config.js
+++ config/site/site.config.js
@@ -125,7 +125,7 @@ const siteConfig = {
     },
 
 
-    /** This section allows to you execute arbitrary html in the head of index.html
+    /** This section allows to you inject arbitrary html into the head tag of index.html.
      *  It takes an array of strings. You could load the string from an external file if desired.
      *  Add like:
      *  headHtml:[
