7bd1dbab59210ea731bcd92922ea410fec356317
Matt Henkes <mjhenkes@gmail.com>
2019-05-16 11:25:06 -0500

    Prepare for toolkit v5

--- src/terra-dev-site/doc/Configuration.b/navigation.config.b.doc.jsx
+++ src/terra-dev-site/doc/Configuration.b/navigation.config.b.doc.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import Markdown from 'terra-markdown';
 import ReadMe from './navigation.config.md';
-// eslint-disable-next-line import/no-webpack-loader-syntax,
+// eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
 import config from '!raw-loader!../../../../config/site/navigation.config';
 
 // We're not using the doc template here to avoid circular dependencies.
