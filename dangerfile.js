0635ddc080093e40d001319617d1a9269c8a9c04
Nathan Faltermeier <nathan.faltermeier@cerner.com>
2019-06-04 07:08:20 -0500

    Fix not checking config folder for changes (#168)
    * Fix not checking config folder for changes
    
    * Escape periods in regex
    
    * Remove big PR warning

--- dangerfile.js
+++ dangerfile.js
@@ -2,12 +2,12 @@
 import { danger, warn, fail, message } from 'danger';
 
 const newChangelog = danger.git.created_files.filter((filePath) => {
-  const srcFilePattern = /CHANGELOG.md/i;
+  const srcFilePattern = /CHANGELOG\.md/i;
   return srcFilePattern.test(filePath);
 });
 
 const modifiedChangelog = danger.git.modified_files.filter((filePath) => {
-  const srcFilePattern = /CHANGELOG.md/i;
+  const srcFilePattern = /CHANGELOG\.md/i;
   return srcFilePattern.test(filePath);
 });
 
@@ -17,7 +17,7 @@ const modifiedSrcFiles = danger.git.modified_files.filter((filePath) => {
 });
 
 const modifiedConfigFiles = danger.git.modified_files.filter((filePath) => {
-  const configFilePattern = /\/src/i;
+  const configFilePattern = /\/config/i;
   return configFilePattern.test(filePath);
 });
 
@@ -34,10 +34,4 @@ const hasModifiedGenerateConfigScriptFiles = modifiedGenerateConfigScriptFiles.l
 // Fail if there are src code changes without a CHANGELOG update
 if ((hasModifiedSrcFiles || hasModifiedConfigFiles || hasModifiedGenerateConfigScriptFiles) && !hasCHANGELOGChanges) {
   fail('Please include a CHANGELOG entry with this PR.');
-}
-
-// Warn when there is a big PR
-const bigPRThreshold = 1000;
-if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR. Consider breaking this into smaller PRs if applicaple');
 }
