9b84a5792a0ca2913047b9bee9ce9868ede36897
Matt Henkes <mjhenkes@gmail.com>
2019-08-12 14:40:55 -0500

    fix index file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const DirectorySwitcherPlugin = require('./plugin/resolve/DirectorySwitcherPlugin');
-const LocalPackageAliasPlugin = require('./plugin/resolve/LocalPackageAliasPlugin');
+const DirectorySwitcherPlugin = require('./config/webpack/plugin/resolve/DirectorySwitcherPlugin');
+const LocalPackageAliasPlugin = require('./config/webpack/plugin/resolve/LocalPackageAliasPlugin');
 const TerraDevSiteEntryPoints = require('./config/webpack/plugin/TerraDevSiteEntryPoints');
 const TerraDevSitePlugin = require('./config/webpack/plugin/TerraDevSitePlugin');
 
