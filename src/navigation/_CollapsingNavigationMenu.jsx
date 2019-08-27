05d3168f568261e1938679f6ab76f292c8c5e9e4
Lokesh P <lokesh.p@cerner.com>
2019-08-27 12:51:00 +0530

    updating imports

--- src/navigation/_CollapsingNavigationMenu.jsx
+++ src/navigation/_CollapsingNavigationMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import KeyCode from 'keycode-js';
+import * as KeyCode from 'keycode-js';
 
 import IconCaretRight from 'terra-icon/lib/icon/IconCaretRight';
 import IconCaretDown from 'terra-icon/lib/icon/IconCaretDown';
