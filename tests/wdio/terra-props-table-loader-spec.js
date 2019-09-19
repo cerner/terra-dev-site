a0e3a5c66188cff72f952d4d08d73a39ee617c51
Matt Henkes <mjhenkes@gmail.com>
2019-09-19 07:43:54 -0500

    fix wdio

--- tests/wdio/terra-props-table-loader-spec.js
+++ tests/wdio/terra-props-table-loader-spec.js
@@ -1,6 +1,9 @@
 Terra.describeViewports('terra-props-table-loader', ['huge'], () => {
   it('checks that terra-props-table-loader works', () => {
+    const viewport = browser.getViewportSize();
+    viewport.height = 2000;
+    browser.setViewportSize(viewport);
     browser.url('/raw/test/terra-dev-site/terra-props-table-loader');
-    Terra.validates.element('#root');
+    Terra.validates.element();
   });
 });
