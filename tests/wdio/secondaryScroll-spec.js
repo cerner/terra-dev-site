563b4dfc0e1afbf59b46a9a1ddd74f6e06d0789e
Gabe Parra <gabe.parra@cerner.com>
2019-10-29 14:59:21 -0500

    Fixed typo in wdio test

--- tests/wdio/secondaryScroll-spec.js
+++ tests/wdio/secondaryScroll-spec.js
@@ -1,11 +1,11 @@
 Terra.describeViewports('secondary scroll', ['tiny', 'huge'], () => {
   it('should scroll to the correct file path based on navigation', () => {
-    browser.url('/secondary-scroll/terra-dev-site/multiple-files/themed');
+    browser.url('/secondary-scroll/terra-dev-site/multiple-files/example-2');
     Terra.validates.element({ selector: '#root' });
   });
 
-  // it('should find and scroll to the correct file in the directory structure', () => {
-  //   browser.url('secondary-scroll/terra-dev-site/parent-folder/child-folder/placeholder-page');
-  //   Terra.validates.element({ selector: '#root' });
-  // });
+  it('should find and scroll to the correct file in the directory structure', () => {
+    browser.url('secondary-scroll/terra-dev-site/parent-folder/child-folder/example');
+    Terra.validates.element({ selector: '#root' });
+  });
 });
