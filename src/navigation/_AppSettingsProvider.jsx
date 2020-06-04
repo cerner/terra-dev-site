afbcf1ea6ed25925468c3d725ac3c0b658ab33e1
Eric McCullough <eric96@live.missouristate.edu>
2020-06-04 10:18:33 -0500

    Update comment to reflect changes

--- src/navigation/_AppSettingsProvider.jsx
+++ src/navigation/_AppSettingsProvider.jsx
@@ -64,8 +64,8 @@ const AppSettingsProvider = ({ settingsConfig, children }) => {
       }
     };
 
-    /* Use default locales or fall back on user configurations configurations.
-     * terra-toolkit v5.21.0 provides configurations from terraI18n, so this reduces the number of places users need to define their locales.
+    /* Use default locales or fall back on default. terra-toolkit v5.21.0 provides configurations from terraI18n, 
+     * so this reduces the number of places users need to define their locales.
      */
     const locales = typeof TERRA_AGGREGATED_LOCALES === 'object' ? TERRA_AGGREGATED_LOCALES : ['en'];
 
