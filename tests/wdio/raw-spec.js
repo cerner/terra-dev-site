5634386a155cf1872b6d998634c0d5df8057ef61
Matt Henkes <mjhenkes@gmail.com>
2020-10-14 11:35:23 -0500

    Fix tests

--- tests/wdio/raw-spec.js
+++ tests/wdio/raw-spec.js
@@ -1,11 +1,11 @@
 Terra.describeViewports('raw route', ['tiny', 'huge'], () => {
   it('checks accessibility on the raw route', () => {
-    browser.url('/raw/secondary-nav-test/terra-dev-site/themed');
+    browser.url('/raw/secondary-nav-test/cerner-terra-dev-site/themed');
     Terra.validates.element({ selector: '#root' });
   });
 
   it('checks that the # route redirects ', () => {
-    browser.url('#/raw/secondary-nav-test/terra-dev-site/themed');
+    browser.url('#/raw/secondary-nav-test/cerner-terra-dev-site/themed');
     Terra.validates.element('hash route', { selector: '#root' });
   });
 
