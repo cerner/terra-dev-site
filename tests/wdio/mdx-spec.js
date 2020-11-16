bb3abad30a850eae5a8c0813300c3e523be0ae56
Matt Henkes <mjhenkes@gmail.com>
2020-11-16 12:37:14 -0600

    Update tests/wdio/mdx-spec.js
    Co-authored-by: Emily Rohrbough  <emilyrohrbough@users.noreply.github.com>

--- tests/wdio/mdx-spec.js
+++ tests/wdio/mdx-spec.js
@@ -1,7 +1,7 @@
 Terra.describeViewports('mdx', ['huge'], () => {
   it('renders mdx', () => {
     browser.url('/raw/test/cerner-terra-dev-site/mdx');
-    browser.moveToObject('#root', 0, 900);
+    browser.moveToObject('#root', 0, 768);
     Terra.validates.element('mdx');
   });
 
