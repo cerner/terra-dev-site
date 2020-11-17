6321ad30a1d077f71e4cc1814e903f342385b52e
Matt Henkes <mjhenkes@gmail.com>
2020-11-17 15:00:19 -0600

    Remove resolve plugin exports.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const DirectorySwitcherPlugin = require('./lib/webpack/plugin/resolve/DirectorySwitcherPlugin');
-const LocalPackageAliasPlugin = require('./lib/webpack/plugin/resolve/LocalPackageAliasPlugin');
 const TerraDevSiteEntrypoints = require('./lib/webpack/plugin/TerraDevSiteEntrypoints');
 const TerraDevSite = require('./lib/webpack/plugin/TerraDevSite');
 
 module.exports = {
-  DirectorySwitcherPlugin,
-  LocalPackageAliasPlugin,
   TerraDevSiteEntrypoints,
   TerraDevSite,
 };
