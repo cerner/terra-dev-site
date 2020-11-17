411ef615a5131d993bb93d2d4da832e672e3deb0
Matt Henkes <mjhenkes@gmail.com>
2020-11-17 09:47:52 -0600

    remove unused toolbar stuff

--- src/pages/_DevSitePage.jsx
+++ src/pages/_DevSitePage.jsx
@@ -1,17 +1,12 @@
 import React, { Suspense } from 'react';
 import Page, { PageActions, Action } from '@cerner/terra-application/lib/page';
 import { NavigationItemContext } from '@cerner/terra-application/lib/layouts';
-// import IconTreemap from 'terra-icon/lib/icon/IconTreemap';
-// import IconLocationPin from 'terra-icon/lib/icon/IconLocationPin';
 import IconStartPresenting from 'terra-icon/lib/icon/IconStartPresenting';
 import { useLocation, useHistory, useRouteMatch } from 'react-router-dom';
 
 import PageErrorBoundary from './_DevSitePageErrorBoundary';
 import LoadingOverlay from './_LoadingOverlay';
 import ContentLoadedContainer from './_ContentLoaded';
-// import AppSettingsContext from '../site/_AppSettingsContext';
-// import ContentSettingsMenu from './_ContentSettingsMenu';
-// import ComponentToolbar from './_ComponentToolbar';
 import NotFoundPage from './_NotFoundPage';
 import { contentImportsPropType, pageContentConfigPropType } from '../site/siteConfigPropTypes';
 
@@ -32,11 +27,6 @@ const DevSitePage = ({ pageContentConfig, contentImports }) => {
   const history = useHistory();
   const isRaw = useRouteMatch('/raw');
   const isHome = useRouteMatch('/home');
-  // const localeButtonRef = React.useRef();
-  // const themeButtonRef = React.useRef();
-  // const [showThemeMenu, setShowThemeMenu] = React.useState(false);
-  // const [showLocale, setShowLocale] = React.useState(false);
-  // const appSettings = React.useContext(AppSettingsContext);
   const { isActive } = React.useContext(NavigationItemContext);
 
   if (!isActive) {
@@ -44,24 +34,12 @@ const DevSitePage = ({ pageContentConfig, contentImports }) => {
   }
 
   const pathname = isRaw ? location.pathname.substring(4) : location.pathname;
-  // const hasThemes = appSettings.themes && appSettings.themes.length > 1;
-  // const hasLocales = appSettings.locales && appSettings.locales.length > 1;
   const ContentComponent = contentImports[pathname];
 
   // Last Chance 404
   if (!ContentComponent) {
     return <NotFoundPage />;
   }
-
-  // const onChangeTheme = (theme) => {
-  //   setShowThemeMenu(false);
-  //   appSettings.onUpdate({ theme });
-  // };
-
-  // const onChangeLocale = (locale) => {
-  //   setShowLocale(false);
-  //   appSettings.onUpdate({ locale });
-  // };
 
   const pageActions = (
     <PageActions>
@@ -74,33 +52,12 @@ const DevSitePage = ({ pageContentConfig, contentImports }) => {
     </PageActions>
   );
 
-  // if (hasThemes) {
-  //   pageActions.push({
-  //     key: 'theme',
-  //     label: 'Theme',
-  //     icon: <IconTreemap />,
-  //     onSelect: () => { setShowThemeMenu(true); },
-  //     buttonRefCallback: (ref) => { themeButtonRef.current = ref; },
-  //   });
-  // }
-
-  // if (hasLocales) {
-  //   pageActions.push({
-  //     key: 'locale',
-  //     label: 'Locale',
-  //     icon: <IconLocationPin />,
-  //     onSelect: () => { setShowLocale(true); },
-  //     buttonRefCallback: (ref) => { localeButtonRef.current = ref; },
-  //   });
-  // }
-
   const props = {};
 
   if (isHome || isRaw) {
     props.preferHeaderIsHidden = true;
   } else {
     props.actions = pageActions;
-    // props.toolbar = <ComponentToolbar />;
   }
 
   return (
@@ -119,28 +76,6 @@ const DevSitePage = ({ pageContentConfig, contentImports }) => {
           </ContentLoadedContainer>
         </Suspense>
       </PageErrorBoundary>
-      {/* { showLocale
-      && (
-        <ContentSettingsMenu
-          text="Locale"
-          items={appSettings.locales}
-          selectedKey={appSettings.currentLocale}
-          onChange={onChangeLocale}
-          targetRef={localeButtonRef.current}
-          onRequestClose={() => setShowLocale(false)}
-        />
-      )}
-      { showThemeMenu
-      && (
-        <ContentSettingsMenu
-          text="Theme"
-          items={appSettings.themes}
-          selectedKey={appSettings.currentTheme}
-          onChange={onChangeTheme}
-          targetRef={themeButtonRef.current}
-          onRequestClose={() => setShowThemeMenu(false)}
-        />
-      )} */}
     </Page>
   );
 };
