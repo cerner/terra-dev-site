961d333c4a78fdc13f896c52c422f2e7a48bc273
Jaime Mackey <jaime.mackey@cerner.com>
2021-06-02 11:46:32 -0500

    wdio config

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,12 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { config } = require('@cerner/terra-functional-testing');
 
-const wdioConfig = config;
-
-const travis = process.env.TRAVIS;
-
-if (travis) {
-  wdioConfig.host = 'localhost';
-}
-
-exports.config = wdioConfig;
+exports.config = config;
